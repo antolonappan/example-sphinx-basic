abef13b8bed8114095e35bf5e019b67f9e7152fb
Anto Idicherian Lonappan <antolonappan@icloud.com>
2024-09-05 02:30:59 -0700

    minor
    Signed-off-by: Anto Idicherian Lonappan <antolonappan@icloud.com>

---
+++ pkg_name/inst/exp.py
@@ -0,0 +1,16 @@
+"""
+This is a module that contains a function that returns the name and age of a person.
+"""
+
+def experiment(name, age):
+    """
+    Return the name and age of the person.
+
+    :param name: The name of the person.
+    :type name: str
+    :param age: The age of the person.
+    :type age: int
+    :return: The name and age of the person.
+    :rtype: str
+    """
+    return f"{name} is {age} years old."
