884b01a2c17a93c29efee4731518eb09b3823331
Anto Idicherian Lonappan <antolonappan@icloud.com>
2024-09-05 02:33:29 -0700

    minor
    Signed-off-by: Anto Idicherian Lonappan <antolonappan@icloud.com>

--- pkg_name/inst/exp.py
+++ pkg_name/inst/exp.py
@@ -14,3 +14,39 @@ def experiment(name, age):
     :rtype: str
     """
     return f"{name} is {age} years old."
+
+
+class Person:
+    """
+    A class that represents a person.
+    """
+
+    def __init__(self, name, age):
+        """
+        Initialize the person with a name and age.
+
+        :param name: The name of the person.
+        :type name: str
+        :param age: The age of the person.
+        :type age: int
+        """
+        self.name = name
+        self.age = age
+    
+    def get_name(self):
+        """
+        Return the name of the person.
+
+        :return: The name of the person.
+        :rtype: str
+        """
+        return self.name
+
+    def __str__(self):
+        """
+        Return a string representation of the person.
+
+        :return: The name and age of the person.
+        :rtype: str
+        """
+        return f"{self.name} is {self.age} years old."
